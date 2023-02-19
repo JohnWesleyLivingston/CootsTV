c2af7b8bd0f067445bdb6cd3c8558d7e171d9dfc
JohnWesleyLivingston <johnwesley.livingston@gmail.com>
2023-02-19 02:10:33 -0500

    Updated game runner

--- Assets/Scripts/GameRunner.cs
+++ Assets/Scripts/GameRunner.cs
@@ -2,80 +2,100 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameRunner : MonoBehaviour
 {
-    public bool isFirstGame;
-    public bool isLastGame;
-
-    public GameObject thisGame;
-    public GameObject nextGame;
     public GameObject transitionScreen;
+
+    public TextMeshProUGUI gameInstructions;
     public GameObject instructionText;
+
+    public GameObject[] gameList;
+    public int currentGame = 0;
+
     public GameObject cam;
     public Transform camPos;
 
-    public float transitionTime = 3.5f;
+    public float transitionTime = 1;
+
+    public MinigameManager minigameManager;
 
     void Start()
     {
-        cam.transform.position = camPos.transform.position;
-        cam.transform.rotation = camPos.transform.rotation;
-
-        if (!isFirstGame)
-        {
-            thisGame.SetActive(true);
-        }
-        else
-        {
-            print("first game");
-        }
-        if (!isLastGame)
-        {
-            nextGame.SetActive(false);
-        }
-        else
-        {
-            print("end screen");
-        }
-
         transitionScreen.SetActive(false);
-        instructionText.SetActive(true);
+        instructionText.SetActive(false);
+
+        for (int i = 0; i < gameList.Length; i++)
+        {
+            gameList[i].SetActive(i == 0);
+        }
     }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            GameComplete();
+        }
+    }
+
+
 
     public void GameComplete()
     {
         StartCoroutine(Countdown());
+    }
 
+    private void RunNextGame()
+    {
+        if (currentGame < gameList.Length -1)
+        {
+
+            gameList[currentGame].SetActive(false);
+            currentGame++;
+            gameList[currentGame].SetActive(true);
+
+            minigameManager = gameList[currentGame].GetComponent<MinigameManager>();
+
+            gameInstructions.text = minigameManager.gameInstructions;
+
+            camPos = minigameManager.startingCamPos;
+            cam.transform.position = camPos.transform.position;
+            cam.transform.rotation = camPos.transform.rotation;
+        }
+        else
+        {
+            print("FINISHED");
+            gameInstructions.text = "FINISHED";
+
+        }
     }
 
     private IEnumerator Countdown()
     {
         transitionScreen.SetActive(true);
-        if (isLastGame)
+
+        yield return new WaitForSeconds(1f);
+
         {
-            yield return new WaitForSeconds(transitionTime);
+            instructionText.SetActive(true);
+
+            RunNextGame();
+
+            yield return new WaitForSeconds(1.5f);
             {
-                SceneManager.LoadScene(0);
-                PlayerPrefs.SetInt("Completed", 2);
-            }
-
-        }
-        else
-        {
-            yield return new WaitForSeconds(transitionTime);
-
-            {
-
-                instructionText.SetActive(false);
-                nextGame.SetActive(true);
                 transitionScreen.SetActive(false);
 
-                thisGame.SetActive(false);
+                yield return new WaitForSeconds(1f);
+                {
+                    instructionText.SetActive(false);
+                    yield return null;
+                }
 
-
-                yield return null;
             }
         }
+
     }
 }
---
+++ Assets/Scripts/MinigameManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinigameManager : MonoBehaviour
+{
+    public string gameInstructions;
+    public Transform startingCamPos;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
